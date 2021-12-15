67acbc4f098259cb539af884377ae0c90515da71
Vadym <loikfit@gmail.com>
2021-12-16 00:17:46 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const refs = {
+  galleryList: document.querySelector('.gallery'), //общий родитель картинок
+};
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
+        />
+      </a>
+    </div>`,
+  )
+  .join('');
+
+refs.galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+function createModal(e) {}
+
 console.log(galleryItems);
