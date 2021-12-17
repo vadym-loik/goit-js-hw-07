531f9dfcd3d3f865e01d780640198a3f363dde00
Vadym <loikfit@gmail.com>
2021-12-18 01:13:22 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+const lightboxOptions = {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const gallery = new SimpleLightbox('.gallery a', lightboxOptions);
+
 console.log(galleryItems);
