ece6b05a7d25801852d2b4ddb54741f5503a558c
Vadym <loikfit@gmail.com>
2021-12-17 23:12:43 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-const refs = {
-  galleryList: document.querySelector('.gallery'), //общий родитель картинок
-};
+const galleryList = document.querySelector('.gallery');
+const bodyElement = document.querySelector('body');
+let modalImage;
 
 const createGalleryMarkup = galleryItems
   .map(
     ({ preview, original, description }) =>
       `<div class="gallery__item">
-      <a class="gallery__link" href="large-image.jpg">
+      <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
-          src=${preview}
-          data-source=${original}
-          alt=${description}
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
         />
       </a>
     </div>`,
   )
   .join('');
 
-refs.galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
 
-function createModal(e) {}
+// document.getElementsByTagName('body')[0].onclick = function (event) {
+//   if (event.target.nodeName === "IMG") {
+//       console.log('do something');
+//   }
+// }
 
-console.log(galleryItems);
+function onImageClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') return;
+
+  onOpenModal(e.target.dataset.source);
+}
+galleryList.addEventListener('click', onImageClick);
+
+const onCreateModal = image =>
+  basicLightbox.create(`<img src = '${image}' width = '2100' alt = '${image}'>`);
+
+const onOpenModal = image => {
+  modalImage = onCreateModal(image);
+  modalImage.show();
+  console.log('Open modal');
+  document.addEventListener('keyup', onKeyPress);
+};
+
+const onKeyPress = e => {
+  if (e.code === 'Escape') modalImage.close();
+  console.log('Close modal with escape');
+  document.removeEventListener('keyup', onKeyPress);
+  console.log(e);
+};
+
+// console.log(galleryItems);
